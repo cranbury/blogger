af1bd729519910c477f5b8129c92bb8451f905da
App Academy Student <student@appacademy.io>
2013-11-10 15:26:21 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '30b380d24d01a600d94361a0915804c1a229d76d3ba7d878f4bf04fb8d903a183da7c7de18fa6acd71705989a41133ab6fd93c4915c189d18f6664d7136008a8'
