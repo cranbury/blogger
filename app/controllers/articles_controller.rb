c262d028b78d09b6ba4080b03899617600e71568
App Academy Student <student@appacademy.io>
2013-11-10 23:38:37 -0500

    finished I1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include
   def index
     @articles = Article.all
   end
@@ -6,4 +7,42 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(
+      title: params[:article][:title],
+      body: params[:article][:body])
+    @article.save
+
+    flash.notice = "Article '#{@article.title}' Created!"
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    flash.notice = "Article '#{@article.title}' Destroyed!"
+
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(params[:article])
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
+  end
+
 end
